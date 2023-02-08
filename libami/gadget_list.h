5760775c326708ca1c19f4c5ae7610eb0defe89b
Adrian Chadd <adrian@FreeBSD.org>
2023-02-07 22:49:41 -0800

    [amiwm] start planning out what to do for gadget events.

--- libami/gadget_list.h
+++ libami/gadget_list.h
@@ -1,14 +1,26 @@
 #ifndef	__GADGET_LIST_H__
 
 struct gadget_def;
+struct gadget_event;
 
+/* X11 events into the list */
 typedef int gadget_list_event_cb(struct gadget_list *list, XEvent *event);
+
+/* Gadget generated events into this list */
+typedef int gadget_list_gevent_cb(struct gadget_list *list, void *cbdata,
+	     struct gadget_event *event);
 
 struct gadget_list {
 	/* Yeah, single link list for now */
 	TAILQ_HEAD(gadget_def_l, gadget_def) list;
 
 	gadget_list_event_cb *event_cb;
+
+	/* gadget generated events */
+	struct {
+		gadget_list_gevent_cb *cb;
+		void *cbdata;
+	 } gevent;
 };
 
 /* Create list */
@@ -22,5 +34,12 @@ extern	void gadget_list_remove(struct gadget_list *list, struct gadget_def *g);
 
 /* Pass an X event into the list, route it to the right widget */
 extern	int gadget_list_handle_event(struct gadget_list *list, XEvent *event);
+
+/*
+ * Pass a gevent event into the list; route it to a matching gadget or
+ * up to the parent to handle/callback.
+ */
+extern	int gadget_list_handle_gevent(struct gadget_list *list,
+	     struct gadget_event *gevent);
 
 #endif	/* __GADGET_LIST_H__ */
