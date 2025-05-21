340d5686544342d41d6234096dd35e6a5e1547b1
Adrian Chadd <adrian@FreeBSD.org>
2025-05-20 19:00:20 -0700

    [libami] document some more of the libami event routing
    I'm slowly figuring out how keyboard events are being routed
    through the libami / event broker system.

--- libami/eventdispatcher.c
+++ libami/eventdispatcher.c
@@ -4,6 +4,12 @@ static int (*eventfunc[LASTEvent])(XEvent*);
 
 static unsigned long eventmask=0;
 
+/**
+ * Dispatch an event.
+ *
+ * If an event is handled by the event function then consume it,
+ * else call cx_send_event() to route it back towards amiwm.
+ */
 static void eb_dispatch(XEvent *e, unsigned long mask)
 {
   if(e->type<0 || e->type>=LASTEvent || (!eventfunc[e->type]) ||
@@ -11,6 +17,14 @@ static void eb_dispatch(XEvent *e, unsigned long mask)
     cx_send_event(mask, e);
 }
 
+/**
+ * Register an event with the amiwm broker.
+ *
+ * This registers the callback for the given event type, and
+ * will add to the current event mask.  cx_broker() will
+ * register the broker with libami and thus amiwm, and amiwm
+ * will route the events here appropriately.
+ */
 void cx_event_broker(int type, unsigned long mask, int (*callback)(XEvent*))
 {
   if(type>=0 && type<LASTEvent) {
